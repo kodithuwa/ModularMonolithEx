40d0ad74bdbfcdf977b4c489b53a6cb33d218310
Your Name <you@example.com>
2024-11-25 11:36:06 +0200

    Moular Monolith Modifcations

--- Common/ReposiotryRegistration.cs
+++ Common/ReposiotryRegistration.cs
@@ -2,34 +2,96 @@
 
 namespace Common
 {
+    using System.ComponentModel;
     using System.Reflection;
+    using System.Runtime.CompilerServices;
     using Microsoft.Extensions.DependencyInjection;
 
     public static class RepositoryRegistration
     {
-        public static IServiceCollection AddRepositoriesFromAssembly(this IServiceCollection services, Assembly assembly)
+        public static IServiceCollection AddRepositoriesFromAssembly(this IServiceCollection services, IEnumerable<Assembly> assemblies)
         {
+            var typeList = new List<Type>();
+            if(assemblies == null || !assemblies.Any())
+            {
+                return services;
+            }
+
+            assemblies.ToList().ForEach(assembly =>
+            {
+                var types = assembly.GetTypes().ToList();
+                typeList.AddRange(types);
+            });
+
+            if (!typeList.Any())
+            {
+                return services;
+            }
+
             // Find all classes implementing IRepository<> or IRepository
-            var repositoryTypes = assembly.GetTypes()
-                .Where(t => t.IsClass && !t.IsAbstract) // Only concrete classes
-                .Where(t => t.GetInterfaces().Any(i =>
-                    //i.IsGenericType && //i.GetGenericTypeDefinition() == typeof(IRepository<>) ||  Generic IRepository<>
-                    i == typeof(IRepository))); // Marker interface IRepository
+            var repositoryTypes = GetAssemblyTypes<IRepository>(typeList);
+
+            // Find all classes implementing IRepository<> or IRepository
+            var serviceTypes = GetAssemblyTypes<IService>(typeList);
+
 
             foreach (var repositoryType in repositoryTypes)
             {
-                var implementedInterfaces = repositoryType.GetInterfaces()
-                    .Where(i => i.IsAssignableFrom(repositoryType) &&
-                        //i.IsGenericType && //i.GetGenericTypeDefinition() == typeof(IRepository<>) || // Generic IRepository<>
-                        i != typeof(IRepository)); // Marker interface IRepository
+                //var implementedInterfaces = repositoryType.GetInterfaces()
+                //    //.Where(i => i.IsAssignableFrom(repositoryType) &&
+                //    //    //i.IsGenericType && //i.GetGenericTypeDefinition() == typeof(IRepository<>) || // Generic IRepository<>
+                //    //    i != typeof(IRepository))
+                //    .Where(i =>
+                //    {
+                //        var interfaces = i.GetInterfaces();
+                //        return interfaces.Any(i => i.Name == "I" + i.Name + "Repository");
 
-                foreach (var implementedInterface in implementedInterfaces)
-                {
-                    services.AddScoped(implementedInterface, repositoryType);
-                }
+                //    }); // Marker interface IRepository
+
+
+                //foreach (var implementedInterface in implementedInterfaces)
+                //{
+                var repoInterface = repositoryType.GetInterface($"I{repositoryType.Name}");
+                services.AddScoped(repoInterface, repositoryType);
+            }
+
+            foreach (var serviceType in serviceTypes)
+            {
+                //var implementedInterfaces = repositoryType.GetInterfaces()
+                //    //.Where(i => i.IsAssignableFrom(repositoryType) &&
+                //    //    //i.IsGenericType && //i.GetGenericTypeDefinition() == typeof(IRepository<>) || // Generic IRepository<>
+                //    //    i != typeof(IRepository))
+                //    .Where(i =>
+                //    {
+                //        var interfaces = i.GetInterfaces();
+                //        return interfaces.Any(i => i.Name == "I" + i.Name + "Repository");
+
+                //    }); // Marker interface IRepository
+
+
+                //foreach (var implementedInterface in implementedInterfaces)
+                //{
+                var serviceInterface = serviceType.GetInterface($"I{serviceType.Name}");
+                services.AddScoped(serviceInterface, serviceType);
             }
 
             return services;
+        }
+
+        public static IEnumerable<Type> GetAssemblyTypes<T>(List<Type> typeList) 
+        {
+            var serviceTypes = typeList
+               .Where(t => t.IsClass && !t.IsAbstract) // Only concrete classes
+               .Where(t => t.GetInterfaces().Any(i =>
+                   //i.IsGenericType && //i.GetGenericTypeDefinition() == typeof(IRepository<>) ||  Generic IRepository<>
+                   i == typeof(T)))
+                .Where(i =>
+                {
+                    var interfaces = i.GetInterfaces();
+                    return interfaces.Any(x => x.Name == "I" + i.Name); //
+
+                });
+            return serviceTypes;
         }
     }
 }
--- Orders/Program.cs
+++ Orders/Program.cs
@@ -1,44 +1,30 @@
 ﻿
 namespace Orders
 {
+    using Common;
     using MediatR;
     using Microsoft.Extensions.DependencyInjection;
-    using Orders.Domain;
-    using Orders.Handlers;
     using Orders.Queries;
-    using Products.Application.Handlers;
-    using Products.Application.Models;
-    using Products.Application.Repositories;
-    using System;
     using System.Reflection;
-    using Common;
 
     public class Program
     {
         private async static Task Main(string[] args)
         {
             var services = new ServiceCollection();
-                   
-
-            services.AddRepositoriesFromAssembly(typeof(Products.Program).Assembly);
+            services.AddRepositoriesFromAssembly(new List<Assembly>{ typeof(Products.Program).Assembly, typeof(Program).Assembly});
 
             var serviceProvider  = services
             .AddMediatR(r => r.RegisterServicesFromAssemblies(typeof(Products.Program).Assembly))
                    .AddMediatR(r => r.RegisterServicesFromAssemblies(typeof(Orders.Program).Assembly))
                .BuildServiceProvider();
             var mediator = serviceProvider.GetRequiredService<IMediator>();
-
             var orderDetails = await mediator.Send(new GetOrderWithProductDetailsQuery(1, 1));
             var result = orderDetails;
 
             //Console.WriteLine(result);
 
             //Console.ReadLine();
-        }
-
-        private static void BuildServiceProvider()
-        {
-            throw new NotImplementedException();
         }
     }
 }
